486f44600357d95c6bcdb7f5565840da15c69cff
Marx Wang <marxw@Marxs-MacBook-Pro.local>
2020-11-22 22:08:15 -0500

    rest dependency

--- homiehomie/scheduler/models.py
+++ homiehomie/scheduler/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    major = models.CharField(max_length=100)
+    name = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
