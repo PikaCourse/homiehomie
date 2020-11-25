4760c1c614b3818520df029611c6a119cdcb4782
Marx Wang <marxw@Marxs-MacBook-Pro.local>
2020-11-25 13:02:45 -0500

    add font, main.css; init Wiki.js

---
+++ homiehomie/frontend/src/components/main/Wiki.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Wiki extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Add Lead Form </h1>{" "}
+      </div>
+    );
+  }
+}
+
+export default Wiki;
