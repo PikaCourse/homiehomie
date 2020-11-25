e4afa354c167921fcc8045feadfb37950d49954f
Marx Wang <marxw@Marxs-MacBook-Pro.local>
2020-11-25 12:53:51 -0500

    redux init

--- homiehomie/frontend/src/components/App.js
+++ homiehomie/frontend/src/components/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Header from './layout/Header'
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+import Header from "./layout/Header";
+import Dashboard from "./main/Dashboard";
 
 class App extends Component {
-    render() {
-        return <Header />;
-    }
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-ReactDOM.render( < App / > , document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
--- homiehomie/frontend/src/components/layout/Header.js
+++ homiehomie/frontend/src/components/layout/Header.js
@@ -4,7 +4,7 @@ export class Header extends Component {
     render() {
         return (
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Homiehomie</a>
+          <a class="navbar-brand" href="#">Homiehomie Beta</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
