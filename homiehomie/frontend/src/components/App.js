92926c23cfea9685e8995480982a16f46fcd709a
Marx Wang <marxw@Marxs-MacBook-Pro.local>
2020-11-25 12:25:42 -0500

    react setup

---
+++ homiehomie/frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+    render()
+    {
+        return <h1>Hello React</h1>;
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- homiehomie/homiehomie/urls.py
+++ homiehomie/homiehomie/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
     path('', include('scheduler.urls')),
-    # path('', TemplateView.as_view(template_name = 'index.html')),
 ]
