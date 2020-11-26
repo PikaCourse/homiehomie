692f758beda4e3a0505f7e103fadbf6e9992182a
William-An <China_Aisa@live.com>
2020-11-26 00:26:30 -0500

    Finish defining scheduler model

--- homiehomie/scheduler/models.py
+++ homiehomie/scheduler/models.py
@@ -1,9 +1,192 @@
 from django.db import models
 
+# TODO Creat user by extending User Model
+from django.contrib.auth.models import User
+
+
+class Schedule(models.Model):
+    """
+    Course scheduled created by USER
+    user:           User creating this schedule
+    created_at:     The time this schedule is created
+    last_edited:    The most recent time this schedule is edited
+    is_star:        Is this schedule starred by user?
+    year:           Year of the schedule for
+    semester:       Semester of the schedule
+    name:           Name of the schedule
+    note:           Note of the schedule
+    tags:           User tagging
+
+    courses:        Access all the courses in this schedule
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now_add=True)
+    is_star = models.BooleanField(default=False)
+    year = models.DecimalField(max_digits=4, decimal_places=0, default=2020)
+    semester = models.CharField(max_length=20)
+    name = models.CharField(max_length=200)
+    note = models.CharField()
+    tags = models.JSONField()
+
+
 # Create your models here.
 class Course(models.Model):
-    major = models.CharField(max_length=100)
-    name = models.CharField(max_length=300)
-    description = models.CharField(max_length=300, default="empty description")
+    """
+    Course data model
+
+    major:          Course major name
+    department:     Course providing department
+    name:           Course name
+    crn:            Course registration number, only number that identified course in a school
+    time:           Course time period, in form of array of JSON containing weekday (0~6),
+                    start_at (HH:MM), end_at (HH:MM)
+    school:         Course provider
+    professor:      Course instructor
+    year:           Course providing year
+    semester:       Course providing semester, possible value: spring, fall, summer
+    description:    Course description
+    tags:           User tagging
+
+    Sample data record:
+    major:          Computer Science
+    department:     School of Science
+    name:           CS 38100
+    crn:            13247-LE1
+    time:           [
+                        {
+                            "weekday": 1,   // Tue
+                            "start_at": "13:30",
+                            "end_at": "14:45"
+                        },
+                        {
+                            "weekday": 3,   // Thursday
+                            "start_at": "13:30",
+                            "end_at": "14:45"
+                        }
+                    ]
+    school:         Purdue University
+    professor:      Tester Test
+    year:           2020
+    semester:       fall
+    description:    Doing some algorithms
+    tags:           ["hard", "interesting", "time-consuming", "math"]
+    """
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    major = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    name = models.CharField(max_length=300)
+    crn = models.CharField(max_length=50)
+    time = models.JSONField()
+    school = models.CharField(max_length=100)
+    professor = models.CharField(max_length=100)
+    year = models.DecimalField(max_digits=4, decimal_places=0, default=2020)
+    semester = models.CharField(max_length=20)
+    description = models.CharField(max_length=2048, default="empty course description")
+    tags = models.JSONField()
+
+    # one to many
+    schedule = models.ForeignKey(Schedule, on_delete=models.PROTECT, related_name="courses")
+
+
+class Question(models.Model):
+    """
+    Question Data model
+    course:         Course this question is referred to
+    created_at:     The time this question is created
+    last_answered:  The most recent time this question is answered
+    like_count:     Like count
+    star_count:     Star/favorite count
+    dislike_count:  Dislike count
+    is_pin:         Is this question pinned by admin?
+    pin_order:      Dictate the order of the question, 0 is the first element
+    title:          Question text
+    tags:           User tagging
+    """
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)    # Prevent deleting course object
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_answered = models.DateTimeField(auto_now_add=True)
+    like_count = models.IntegerField(default=0)
+    star_count = models.IntegerField(default=0)
+    dislike_count = models.IntegerField(default=0)
+    is_pin = models.BooleanField(default=False)
+    pin_order = models.IntegerField(default=0)
+    title = models.CharField(max_length=200)
+    tags = models.JSONField()
+
+
+# TODO Enable referring to past editing via git?
+class Note(models.Model):
+    """
+    Share note Data model
+    course:         Course this note is referred to
+    question:       Question this note is referred to
+    created_at:     The time this question is created
+    last_edited:    The most recent time this question is edited
+    like_count:     Like count
+    star_count:     Star/favorite count
+    dislike_count:  Dislike count
+    title:          Note title
+    content:        Note content in markdown
+    tags:           User tagging
+    """
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)    # Prevent deleting course object
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now_add=True)
+    like_count = models.IntegerField(default=0)
+    star_count = models.IntegerField(default=0)
+    dislike_count = models.IntegerField(default=0)
+    title = models.CharField(max_length=200)
+    content = models.CharField()    # In markdown
+    tags = models.JSONField()
+
+
+class Post(models.Model):
+    """
+    Post Data model
+    course:         Course this note is referred to
+    poster:         User that post this post
+    created_at:     The time this post is created
+    last_edited:    The most recent time this post is edited
+    like_count:     Like count
+    star_count:     Star/favorite count
+    dislike_count:  Dislike count
+    title:          Post title
+    content:        Post content in markdown
+    tags:           User tagging
+    """
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)    # Prevent deleting course object
+    poster = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now_add=True)
+    like_count = models.IntegerField(default=0)
+    star_count = models.IntegerField(default=0)
+    dislike_count = models.IntegerField(default=0)
+    title = models.CharField(max_length=200)
+    content = models.CharField()  # In markdown
+    tags = models.JSONField()
+
+
+class PostAnswer(models.Model):
+    """
+    Post answer/followup Data model
+    post:           Post this answer is referred to
+    postee:         User that answer the post
+    created_at:     The time this answer is created
+    last_edited:    The most recent time this answer is edited
+    like_count:     Like count
+    star_count:     Star/favorite count
+    dislike_count:  Dislike count
+    content:        Post answer content in markdown
+    tags:           User tagging
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    postee = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now_add=True)
+    like_count = models.IntegerField(default=0)
+    star_count = models.IntegerField(default=0)
+    dislike_count = models.IntegerField(default=0)
+    content = models.CharField()  # In markdown
+
