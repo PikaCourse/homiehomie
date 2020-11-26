d4348447839f2d7f0bf44636a37e5c2cd98e9eee
William-An <China_Aisa@live.com>
2020-11-26 00:58:17 -0500

    Register model for admin page

--- homiehomie/scheduler/admin.py
+++ homiehomie/scheduler/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from homiehomie.scheduler.models import *
 
 # Register your models here.
+admin.site.register(Schedule)
+admin.site.register(Course)
+admin.site.register(Question)
+admin.site.register(Note)
+admin.site.register(Post)
+admin.site.register(PostAnswer)
