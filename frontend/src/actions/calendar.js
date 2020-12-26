3a0381b9f31e6d3212dd3b618288b9ef373a9609
Marx <boyuan@vt.edu>
2020-12-26 12:53:40 -0500

    remove course

--- frontend/src/actions/calendar.js
+++ frontend/src/actions/calendar.js
@@ -1,16 +1,35 @@
-import {ADD_COURSE_TO_CAL, REMOVE_COURSE_FROM_CAL} from './types'
+import {ADD_COURSE_TO_CAL, REMOVE_COURSE_FROM_CAL, UPDATE_COURSE_IN_CAL} from './types'
 import store from '../store'
 
 export const addCurrCourse = ()  =>
 {
+    const courseArray = store
+      .getState()
+      .calendar.calendarCourseBag.filter(
+        (item) => item.raw.selectedCourseArray == store.getState().course.selectedCourseArray
+    );
+    
+    if(!Array.isArray(courseArray) || !courseArray.length) 
+    {
+        // add new course
+        return {
+            type: ADD_COURSE_TO_CAL,
+            selectedCRN: store.getState().course.selectedCRN,
+            selectedCourseArray: store.getState().course.selectedCourseArray,
+        };  
+    }
+    else
+    {
+        // add same course different crn
+        return {
+            type: UPDATE_COURSE_IN_CAL,
+            selectedCRN: store.getState().course.selectedCRN,
+            selectedCourseArray: store.getState().course.selectedCourseArray,
+        };  
 
-    return {
-        type: ADD_COURSE_TO_CAL,
-        selectedCRN: store.getState().course.selectedCRN,
-        selectedCourseArray: store.getState().course.selectedCourseArray,
-    };  
+    }
+    
 }
-
 
 
 export const removeCurrCourse = ()  =>
