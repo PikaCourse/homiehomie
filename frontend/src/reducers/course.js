7fdd041b1c6f51127c8e331c03b299721c184b60
Marx <boyuan@vt.edu>
2020-12-26 10:33:08 -0500

    store course selected course to crn

--- frontend/src/reducers/course.js
+++ frontend/src/reducers/course.js
@@ -3,14 +3,15 @@ import {GET_COURSE, SET_COURSE} from '../actions/types.js'
 
 const initialState = {
     selectedCourseArray:[],
-    selectedCourse:{}
+    selectedCourse:{},
+    selectedCrn:0
 }
 
 export default function(state = initialState, action) {
     switch (action.type) {
         case GET_COURSE:
-            console.log(action.payload);
             return {
+                selectedCrn:action.payload[0].crn,
                 selectedCourse: action.payload[0],
                 selectedCourseArray: action.payload
             } 
