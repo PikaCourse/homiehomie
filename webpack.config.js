04ebb0cdd080e34389a84bb99289b216e1cbac6f
MARX1108 <boyuan@vt.edu>
2020-12-27 10:32:43 -0500

    Wishlist 2.0

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,21 @@ module.exports = {
                 test: /\.(scss)$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
 
-              },
+            },
+            {
+                test: /\.less$/,
+                loader: "less-loader", // compiles Less to CSS
+                options: {
+                           lessOptions: { // If you are using less-loader@5 please spread the lessOptions to options directly
+                             modifyVars: {
+                               'primary-color': 'rgb(65, 158, 244)',
+                               'link-color': 'rgb(65, 158, 244)',
+                               'border-radius-base': '2px',
+                             javascriptEnabled: true,
+                           },
+                         },
+                    },
+            },
         ]
     }
 }
