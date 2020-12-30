f377db9bab20c4bb41f7a6ce32ac135edcd1cddd
Marx <boyuan@vt.edu>
2020-12-29 20:58:48 -0500

    update

--- frontend/src/reducers/question.js
+++ frontend/src/reducers/question.js
@@ -6,63 +6,6 @@ const initialState = {
             id:0,
             question:{},
             note:[]
-        },
-        {
-            id:0,
-            question:{
-                "id": 0,
-                "course_meta": 0,
-                "created_by": 0,
-                "created_at": "string",
-                "last_answered": "string",
-                "last_edited": "string",
-                "like_count": 0,
-                "star_count": 0,
-                "dislike_count": 0,
-                "is_pin": true,
-                "pin_order": 0,
-                "title": "string",
-                "tags": [
-                  "string"
-                ]
-            },
-            note:[
-                {
-                "course": 0,
-                "question": 0,
-                "id": 0,
-                "created_at": "string",
-                "last_edited": "string",
-                "like_count": 0,
-                "star_count": 0,
-                "dislike_count": 0,
-                "title": "string",
-                "content": "string",
-                "tags": [
-                  "string"
-                ]
-              },
-              {
-                "course": 0,
-                "question": 0,
-                "id": 0,
-                "created_at": "string",
-                "last_edited": "string",
-                "like_count": 0,
-                "star_count": 0,
-                "dislike_count": 0,
-                "title": "string",
-                "content": "string",
-                "tags": [
-                  "string"
-                ]
-              }
-            ]
-        },
-        {
-            id:1,
-            question:[],
-            note:[]
         }
     ]
 }
