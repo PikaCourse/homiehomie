a61b5b9a91959852c76f1da7c76beb214805b298
Marx <boyuan@vt.edu>
2020-12-29 22:29:30 -0500

    cleanup

--- frontend/src/reducers/question.js
+++ frontend/src/reducers/question.js
@@ -1,4 +1,4 @@
-import {GET_NOTES, GET_QUE} from '../actions/types.js'
+import {GET_QUE} from '../actions/types.js'
 
 const initialState = {
     noteBag:[
@@ -17,27 +17,6 @@ export default function(state = initialState, action) {
     //console.log(action.payload);
 
     switch (action.type) {
-        // case GET_QUE:
-        //     console.log("from question.js: "+action.payload)
-        //     //action.payload [{q1}, {q2}]
-        //     let newBag = [];
-        //     action.payload.map((item, index) => 
-        //     {
-        //         newBag.push(
-        //             {
-        //                 id: index, 
-        //                 question: item,
-        //                 notes:[]
-        //             }
-        //         )
-        //     }
-        //     );
-
-        //     return {
-        //         //add not
-        //         noteBag: newBag
-        //         //@post: notebag 
-        //     } 
         case GET_QUE:
             //action.payload: note array to one question
             let newBag2 = {...state.noteBag};
