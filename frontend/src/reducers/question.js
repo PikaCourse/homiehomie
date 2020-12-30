01bda862e8fc5c57a362dd7e5df8e6913fc1d31f
Marx <boyuan@vt.edu>
2020-12-29 19:48:58 -0500

    noteBag

--- frontend/src/reducers/question.js
+++ frontend/src/reducers/question.js
@@ -1,11 +1,62 @@
 import {GET_QUE} from '../actions/types.js'
 
 const initialState = {
-    question:[
+    noteBag:[
         {
             id:0,
+            question:{
+                "id": 0,
+                "course_meta": 0,
+                "created_by": 0,
+                "created_at": "string",
+                "last_answered": "string",
+                "last_edited": "string",
+                "like_count": 0,
+                "star_count": 0,
+                "dislike_count": 0,
+                "is_pin": true,
+                "pin_order": 0,
+                "title": "string",
+                "tags": [
+                  "string"
+                ]
+              },
+            note:[{
+                "course": 0,
+                "question": 0,
+                "id": 0,
+                "created_at": "string",
+                "last_edited": "string",
+                "like_count": 0,
+                "star_count": 0,
+                "dislike_count": 0,
+                "title": "string",
+                "content": "string",
+                "tags": [
+                  "string"
+                ]
+              },
+              {
+                "course": 0,
+                "question": 0,
+                "id": 0,
+                "created_at": "string",
+                "last_edited": "string",
+                "like_count": 0,
+                "star_count": 0,
+                "dislike_count": 0,
+                "title": "string",
+                "content": "string",
+                "tags": [
+                  "string"
+                ]
+              }
+            ]
+        },
+        {
+            id:1,
             question:[],
-            notes:[]
+            note:[]
         }
     ]
 }
