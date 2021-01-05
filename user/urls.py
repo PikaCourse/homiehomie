93011a6a902bef42ee5f5ac5a15eab85092415e3
William-An <China_Aisa@live.com>
2021-01-04 20:24:35 -0500

    Increase field size for professor to temporarily allow multiple professors, need better sol

--- user/urls.py
+++ user/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from user.views import *
 
 urlpatterns = [
-    path('login/',
+    path('login',
          LoginView.as_view(template_name="user/login.html",
                            redirect_authenticated_user=True),
          name='login'),
-    path('register/', RegisterUserView.as_view(), name='register'),
+    path('register', RegisterUserView.as_view(), name='register'),
     # path('', include('django.contrib.auth.urls')),
     # path('', include(router.urls)),
 ]
