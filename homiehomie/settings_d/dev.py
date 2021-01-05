2de241cd635a7507ea8e8ba0305cc5b87fd8afc8
William-An <China_Aisa@live.com>
2021-01-05 15:52:10 -0500

    Disable debug for dev server

--- homiehomie/settings_d/dev.py
+++ homiehomie/settings_d/dev.py
@@ -13,7 +13,7 @@ from homiehomie.settings_d.default import *
 
 ALLOWED_HOSTS = ["test-homiehomie.thexyzlab.studio", "127.0.0.1", "localhost"]
 
-DEBUG = True
+DEBUG = False
 
 DATABASES = {
     'default': dj_database_url.parse(config("DATABASE_URL")),
