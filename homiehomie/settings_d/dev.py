dc84da95cd69b213ea6503da76d1d554da984b75
William-An <China_Aisa@live.com>
2021-01-05 15:51:03 -0500

    Use whitenoise to serve static regardless of dev or prod or local

--- homiehomie/settings_d/dev.py
+++ homiehomie/settings_d/dev.py
@@ -15,17 +15,6 @@ ALLOWED_HOSTS = ["test-homiehomie.thexyzlab.studio", "127.0.0.1", "localhost"]
 
 DEBUG = True
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 DATABASES = {
     'default': dj_database_url.parse(config("DATABASE_URL")),
 }
