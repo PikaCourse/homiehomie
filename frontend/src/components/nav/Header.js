2de01f947cd39d35edd866d222cedc00c8491c11
MARX1108 <boyuan@vt.edu>
2021-01-07 11:27:23 -0500

    star button

--- frontend/src/components/nav/Header.js
+++ frontend/src/components/nav/Header.js
@@ -283,47 +283,54 @@ function Header() {
   }
 
   return (
-    // <form action="/user/login" method="post">
-    //   <button type="submit">Send</button>
-    // </form>
     <nav className="navbar navbar-expand-lg navbar-light bg-light border-0 pb-2 pt-2">
       <div className="container-fluid">
         <a className="navbar-brand mx-4" href="#" style={selectedStyle}>
           CourseWiki
         </a>
-
-        <Button type="primary" onClick={showModal}>
-          Login
-        </Button>
-        <Modal
-          visible={visible}
-          title="Title"
-          onOk={handleOk}
-          onCancel={handleCancel}
-          footer={null}
-        >
-          <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={login ? loginSubmit : signupSubmit}
-            // onFinishFailed={}
-          >
-            <Form.Item label="">
-              <Radio.Group
-                onChange={onFormTypeChange}
-                value={login ? "login" : "signup"}
+        <div>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <Button size="medium" style={{ color: "#419EF4"}}>
+                <FontAwesomeIcon icon={faStar} />
+              </Button>
+            </li>
+            <li class="nav-item">
+              <Button type="primary" onClick={showModal}>
+                Login
+              </Button>
+              <Modal
+                visible={visible}
+                title="Title"
+                onOk={handleOk}
+                onCancel={handleCancel}
+                footer={null}
               >
-                <Radio.Button value="login">Login</Radio.Button>
-                <Radio.Button value="signup">Sign Up</Radio.Button>
-              </Radio.Group>
-            </Form.Item>
-            {login ? loginForm : signupForm}
-            {login ? loginFooter : signupFooter}
-          </Form>
-        </Modal>
+                <Form
+                  name="normal_login"
+                  className="login-form"
+                  initialValues={{
+                    remember: true,
+                  }}
+                  onFinish={login ? loginSubmit : signupSubmit}
+                  // onFinishFailed={}
+                >
+                  <Form.Item label="">
+                    <Radio.Group
+                      onChange={onFormTypeChange}
+                      value={login ? "login" : "signup"}
+                    >
+                      <Radio.Button value="login">Login</Radio.Button>
+                      <Radio.Button value="signup">Sign Up</Radio.Button>
+                    </Radio.Group>
+                  </Form.Item>
+                  {login ? loginForm : signupForm}
+                  {login ? loginFooter : signupFooter}
+                </Form>
+              </Modal>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
