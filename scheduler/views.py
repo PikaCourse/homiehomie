32bb39cefee34507b5d97d413deaaa922d27cac9
William-An <China_Aisa@live.com>
2021-01-07 23:33:33 -0500

    Put pinned question to first

--- scheduler/views.py
+++ scheduler/views.py
@@ -169,9 +169,9 @@ class QuestionViewSet(viewsets.ModelViewSet):
         if sortby is not None:
             if sortby not in self.supported_sortby_options:
                 raise InvalidQueryValue()
-            queryset = queryset.order_by(("-" if descending else "") + sortby, "-last_answered")
+            queryset = queryset.order_by("-is_pin", "pin_order", ("-" if descending else "") + sortby, "-last_answered")
         else:
-            queryset = queryset.order_by(("-" if descending else "") + "like_count", "-last_answered")
+            queryset = queryset.order_by("-is_pin", "pin_order", ("-" if descending else "") + "like_count", "-last_answered")
         # Also sub order by created time, the newest is at top
         if limit is not None:
             try:
