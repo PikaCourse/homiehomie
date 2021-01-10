ebd8a3c4c328a6eda0f79f561951e257648be6d9
William-An <China_Aisa@live.com>
2021-01-10 05:12:13 -0500

    Fix bug in action script to build main.js [deploy]

--- sitecustomize.py
+++ sitecustomize.py
@@ -4,7 +4,7 @@ created at:  01/10/2021
 author:      Weili An
 email:       china_aisa@live.com
 version:     v1.0.0
-desc:        File to enable coverage for sub-process Django test
+desc:        File to enable coverage for sub-process Django unittest
 """
 
 import coverage
