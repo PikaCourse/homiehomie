113c5018d6a5085fb75f78e581c927fc3d20cfba
William-An <China_Aisa@live.com>
2021-01-10 20:46:12 -0500

    Customized destory function for schedule and wishlist

--- scheduler/views.py
+++ scheduler/views.py
@@ -601,6 +601,13 @@ class ScheduleViewSet(viewsets.ModelViewSet):
         user = self.request.user
         return Schedule.objects.filter(student__user=user)
 
+    def destroy(self, request, *args, **kwargs):
+        schedule = self.get_object()
+        self.perform_destroy(schedule)
+        error_pack = {"code": "success", "detail": "successfully deleted schedule",
+                      "schedule": schedule.id, "status": status.HTTP_200_OK}
+        return Response(error_pack)
+
 
 class WishListViewSet(viewsets.ModelViewSet):
     serializer_class = WishListSerializer
@@ -618,3 +625,9 @@ class WishListViewSet(viewsets.ModelViewSet):
     #     # Since only one wishlist for one user, just redirect it
     #     return super().retrieve(request, *args, **kwargs)
 
+    def destroy(self, request, *args, **kwargs):
+        wishlist = self.get_object()
+        self.perform_destroy(wishlist)
+        error_pack = {"code": "success", "detail": "successfully deleted wishlist",
+                      "wishlist": wishlist.id, "status": status.HTTP_200_OK}
+        return Response(error_pack)
