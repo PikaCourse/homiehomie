08f6d9c957f2fe746f2a125fe56a2207fa3295bc
William-An <China_Aisa@live.com>
2021-01-12 23:19:25 -0500

    Set up email service config for mailgun

--- homiehomie/settings_d/dev.py
+++ homiehomie/settings_d/dev.py
@@ -20,8 +20,11 @@ DATABASES = {
 }
 
 # Email setting
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = config("EMAIL_HOST", default="localhost")
 EMAIL_PORT = config("EMAIL_PORT", default=25, cast=int)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False, cast=bool)
 DEFAULT_FROM_EMAIL = "bot@test-homiehomie.thexyzlab.studio"
 
 # Verification token timeout in seconds
