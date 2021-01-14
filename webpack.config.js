f7a368507ccb5d7375d6d67866cba4fbf90f29db
Marx <boyuan@vt.edu>
2021-01-14 01:47:48 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,49 @@
 const path = require("path");
 
 module.exports = {
-    module:{
-        rules:[
-            {
-                test: /\.js$|tsx/,
-                exclude:/node_modules/,
-                use:{
-                    loader:"babel-loader"
-                }
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$|tsx/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "source-map-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "less-loader", // compiles Less to CSS
+            options: {
+              lessOptions: {
+                // If you are using less-loader@5 please spread the lessOptions to options directly
+                modifyVars: {
+                  hack: `true; @import "${path.resolve(
+                    __dirname,
+                    "./frontend/src/",
+                    "main.less"
+                  )}";`,
+                },
+                javascriptEnabled: true,
+              },
             },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(scss)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-
-            },
-            {
-                test: /\.less$/,
-                use: [{
-                    loader: 'style-loader',
-                  }, {
-                    loader: 'css-loader', // translates CSS into CommonJS
-                  }, {
-                    loader: 'less-loader', // compiles Less to CSS
-                    options: {
-                    lessOptions: { // If you are using less-loader@5 please spread the lessOptions to options directly
-                        modifyVars: {
-                            hack: `true; @import "${path.resolve(
-                                __dirname,
-                                "./frontend/src/",
-                                "main.less"
-                                )}";`
-                          },
-                       javascriptEnabled: true,
-                     },
-                   },
-                  }],
-            },
-        ]
-    }
-}
+          },
+        ],
+      },
+    ],
+  },
+};
