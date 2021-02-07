1725a01ab74de7257f68f6f5d98ff1947ea0e72f
William-An <China_Aisa@live.com>
2021-02-06 20:09:02 -0500

    Finish storing messages in db

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,9 +10,10 @@ desc:        Consumer handler for real time chat
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
+from django.utils import timezone
 
 from scheduler.models import CourseMeta
-from chat.serializers import CourseMetaChatSerializer
+from chat.serializers import *
 
 
 class CourseChatConsumer(AsyncWebsocketConsumer):
@@ -23,8 +24,17 @@ class CourseChatConsumer(AsyncWebsocketConsumer):
     3. Receiving incoming message and broadcast to all other ws connections in the same group/course
     """
     async def connect(self):
+        # Get user info from Django session
+        self.user = self.scope["user"]
+        # Assign the db anonymous user if the request is anonymous
+        if self.user.is_anonymous:
+            self.user = await database_sync_to_async(Student.get_anonymous_user)()
+        self.user_data = await self.get_user_data()
+
+        # Get course meta info from path
         course_meta_id = self.scope['url_route']['kwargs']['course_meta_id']
         self.course_meta = await self.get_course_meta(course_meta_id)
+        self.course_meta_data = CourseMetaChatSerializer(self.course_meta).data
         if self.course_meta is None:
             # Invalid course meta id, reject
             await self.close(code=1008)
@@ -38,6 +48,10 @@ class CourseChatConsumer(AsyncWebsocketConsumer):
             )
 
             await self.accept()
+
+    @database_sync_to_async
+    def get_user_data(self):
+        return UserChatSerializer(self.user).data
 
     @database_sync_to_async
     def get_course_meta(self, coursemetaid):
@@ -56,8 +70,15 @@ class CourseChatConsumer(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data):
+        # Inject sender and course info into messsage field
         message = json.loads(text_data)
-        message["course"] = CourseMetaChatSerializer(self.course_meta).data
+        message["course"] = self.course_meta_data
+        message["user"] = self.user_data
+        # Override message timestamp
+        message["message"]["timestamp"] = timezone.now().isoformat()
+
+        # Save the message for history query
+        await self.save_message_to_db(message)
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -67,6 +88,22 @@ class CourseChatConsumer(AsyncWebsocketConsumer):
                 'message': message
             }
         )
+
+    @database_sync_to_async
+    def save_message_to_db(self, message):
+        """
+        Save a message to db with given sender and course meta info
+        :param message: message to be saved
+        :return:
+        """
+        message = CourseChatMessageSerializer(data=message,
+                                              context={"user": self.user,
+                                                       "course_meta": self.course_meta})
+        if message.is_valid(raise_exception=False):
+            message.save()
+        else:
+            # TODO Error handling?
+            pass
 
     # Receive message from room group
     async def chat_message(self, event):
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,33 @@
+"""
+filename:    models.py
+created at:  02/6/2021
+author:      Weili An
+email:       china_aisa@live.com
+version:     v1.0.0
+desc:        Data models for chat
+"""
+
+
 from django.db import models
+from user.models import Student
+from scheduler.models import CourseMeta
 
 # Create your models here.
+
+
+# TODO DB backend switched to Redis for performance improvement when the db is larged?
+class CourseChatMessage(models.Model):
+    """
+    CourseChatMessage data model
+
+    created_at:         Time and date this message is created/received in server
+    is_anonymous:       Is the sender prefer to send this in anonymous mode
+    user:               User who send the message, could be anonymous user (i.e. not authenticated)
+    course_meta:        Course meta object this message relates to
+    message:            Actual message content, defined by frontend
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_anonymous = models.BooleanField(default=False)
+    user = models.ForeignKey(Student, on_delete=models.SET(Student.get_sentinel_user))
+    course_meta = models.ForeignKey(CourseMeta, on_delete=models.CASCADE, default=-1)
+    message = models.JSONField(default=dict)
