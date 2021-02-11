daf2043d5f0e2db808eac93a3aaa22606f3a1fff
kimonazj <kimonazj@users.noreply.github.com>
2021-02-11 08:08:22 +0800

    Landing Page
    Landing Page with an image and one line description. The content is not done

---
+++ frontend/src/landing/index.js
@@ -0,0 +1,13 @@
+/**
+ * File name:	index.js
+ * Created:	02/09/2021
+ * Author:	Ji Zhang
+ * Email:	anna_zhangji@outlook.com
+ * Version:	1.0 Initial file
+ * Description:	Entry point for landing page
+ */
+
+// TODO Folder structure
+
+import landing from "./landing";
+export default landing;
