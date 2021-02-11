bacb4a95498ff2da33edb3164dbe0e9b7eff27e6
joannafg <joannafg@vt.edu>
2021-02-11 16:42:35 -0500

    search bar modal finish (still need documentation)

--- frontend/src/course/components/SearchCourseModal.js
+++ frontend/src/course/components/SearchCourseModal.js
@@ -8,76 +8,21 @@
  */
 
 import React, { useState } from "react";
-import { Input, AutoComplete, Modal} from "antd";
+import { Input, AutoComplete, Modal, Button} from "antd";
 import { getCourseSections, getCourses, clearCourses } from "../action";
 import { useDispatch, useSelector } from "react-redux";
 import { SearchOutlined } from "@ant-design/icons";
 import store from "../../store";
 import { timeObjFommatter, weekday, Color } from "../../helper/global";
+import WikiSummary from "./Summary"
 
 
 function SearchCourseModal(props) {
-    const showModal = () => {
-        props.setIsModalVisible(true);
-    };
-
-    function content() {
-        if (props.isModalVisible) {
-            return (
-                <>
-                <p>Change </p>
-                <h1>{store.getState().course.selectedCourse.course_meta.title}</h1>
-                <h2>{store.getState().course.selectedCourse.course_meta.name}</h2>
-                {tagLoader(store.getState().course.selectedCourse)}
-                <p>Prof: {store.getState().course.selectedCourse.professor}</p>
-                {/* <p>Days: {store.getState().course.selectedCourse.time}</p> */}
-                {/* <p>Time: {store.getState().course.selectedCourse.time}</p> */}
-                <p>Loc: {store.getState().course.selectedCourse.location}</p>
-                </>
-            ); 
-        }
-    }
-
-    //Copy from Summary.js, motification made 
-    function tagLoader(selectedCourse) {
     return (
-        <div>
-        <p className="my-2" style={{ fontFamily: "Montserrat" }}>
-            {
-            weekday.map((day, i) => (
-                <span 
-                key={i}
-                className={weekdayToClass(i, selectedCourse.time)}>
-                {day}
-                </span>
-            ))
-            }
-    
-            <span className="ml-2 mb-1 badge bg-secondary">
-            {selectedCourse.crn == null
-                ? selectedCourse.section
-                : "CRN:"+selectedCourse.crn}
-            </span>
-        </p>
-        </div>
-    );
-    }
-
-    //Copy from Summary.js 
-    function weekdayToClass(index, timeArray) {
-    let timecp = timeArray;
-    let result = "badge bg-light mb-1";
-    timecp.map((timeObj) => {
-        if (timeObj.weekday == index) {
-        result = "mb-1 badge bg-secondary";
-        }
-    });
-    return result;
-    }
-    return (
-    <Modal visible={props.isModalVisible} onOk={props.closeModal} onCancel={props.closeModal}>
-        {props.isModalVisible?content():null}
+    <Modal visible={props.isModalVisible} onOk={props.closeModal} onCancel={props.closeModal} footer={<Button onClick={props.closeModal}>View More</Button>}>
         {/* TODO create when isModalVisible is true, destroy when it is false */}
+        <WikiSummary/>
+        
     </Modal>
     );
 }
