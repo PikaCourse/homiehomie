fa4b26de821994125b802ede521881755d65ad7d
William-An <China_Aisa@live.com>
2021-02-14 02:03:58 -0500

    Finish register form

--- frontend/src/user/components/LoginRegister.js
+++ frontend/src/user/components/LoginRegister.js
@@ -8,7 +8,7 @@
  */
 
 import React, { useState, useEffect } from "react";
-import { Modal, Button, Form, Input, Space } from "antd";
+import { Modal, Button, Form, Input, Space, Checkbox } from "antd";
 import axios from "axios";
 // Config axios to get the csrf cookie preventing manually
 // adding it
@@ -80,7 +80,7 @@ function UserLoginForm(props) {
   const switchForm = props.switchForm;
 
   // Get form instance
-  const [form] = Form.useForm();
+  const [ form ] = Form.useForm();
 
   // Form control state
   const [ isLoading, setIsLoading ] = useState(false);
@@ -153,12 +153,22 @@ function UserRegisterForm(props) {
   // Inherited state control
   const setLogin = props.setLogin;
   const setUserInfo = props.setUserInfo;
+  const switchForm = props.switchForm;
 
   // Get form instance
-  const [form] = Form.useForm();
+  const [ form ] = Form.useForm();
 
   // Form control state
   const [ isLoading, setIsLoading ] = useState(false);
+
+  // User Agreement Modal control
+  const [isUAModalVisible, setUAModalVisible] = useState(false);
+  const setUACheck = (isChecked) => {
+    form.setFields([{
+      name: "agreement",
+      value: isChecked
+    }]);
+  };
 
   // OnSubmit callback
   function submitForm(values) {
@@ -169,13 +179,134 @@ function UserRegisterForm(props) {
   }
 
   return (
-    <Form
-      name="user_register"
-      form={form}
-      onFinish={submitForm}
-    >
-
-    </Form>
+    <>
+      <Form
+        name="user_register"
+        form={form}
+        onFinish={submitForm}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            }
+          ]}
+        >
+          <Input placeholder="e.g. Kakapi" />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              pattern: /^.*@.*\.edu$/,
+              message: "Please input a valid .edu email!"
+            }
+          ]}
+        >
+          <Input placeholder="e.g. pika@course.edu" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            }
+          ]}
+        >
+          <Input.Password
+            placeholder="input password"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Confirm Password"
+          name="confirm_password"
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject("The two passwords that you entered do not match!");
+              },
+            }),
+          ]}
+        >
+          <Input.Password
+            placeholder="input password, again..."
+          />
+        </Form.Item>
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              required: true,
+              validator: (_, value) =>
+                value ? Promise.resolve() : Promise.reject("Please read and accept agreement"),
+            },
+          ]}
+        >
+          <Checkbox>
+            I have read the 
+            <a href=""
+              onClick={(e) => {
+                e.preventDefault();
+                setUAModalVisible(true);
+              }}
+            >
+              &nbsp;agreement
+            </a>
+          </Checkbox>
+        </Form.Item>
+        <Form.Item>
+          <Space>
+            <Button type="primary" htmlType="submit" loading={isLoading}>
+              Create
+            </Button>
+            <>
+              Already have an account?
+              <a href="#" onClick={(e) => {
+                e.preventDefault();
+                switchForm();
+              }}>
+              log in here
+              </a>
+            </>
+          </Space>
+        </Form.Item>
+      </Form>
+      {
+        // TODO Implement the functionality that
+        // TODO Clicking ok on modal will agree the agreement in outside form
+      }
+      <Modal
+        title="PikaCourse User Agreement"
+        visible={isUAModalVisible}
+        okText="I agree"
+        cancelText="I disagree"
+        onOk={() => {
+          setUACheck(true);
+          setUAModalVisible(false);
+        }}
+        onCancel={() => {
+          setUACheck(false);
+          setUAModalVisible(false);
+        }}
+      >
+        User Agreement for PikaCourse
+      </Modal>
+    </>
   );
 }
 
