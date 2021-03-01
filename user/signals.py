070702bf228b1e24772a01c8ca7de6183fe878f5
William-An <China_Aisa@live.com>
2021-03-01 01:25:55 -0500

    Finish notification consumer

--- user/signals.py
+++ user/signals.py
@@ -15,6 +15,29 @@ from scheduler.models import PostAnswer
 from scheduler.serializers import PostAnswerSerializer
 from chat.models import CourseChatMessage
 from chat.serializers import CourseChatMessageSerializer
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+import threading
+
+
+class NotificationThread(threading.Thread):
+    def __init__(self, instance, **kwargs):
+        self.instance = instance
+        super(NotificationThread, self).__init__(**kwargs)
+
+    def run(self):
+        instance = self.instance
+        post_receiver = instance.post.poster
+        content = PostAnswerSerializer(instance).data
+
+        # Create notification in db
+        Notification.objects.create(receiver=post_receiver, content=content)
+
+        # Send out real-time notification to websocket if exist
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(f"notification_{post_receiver.user.username}",
+                                                {"type": "send_notification", "notification": content})
+
 
 # Create student instance upon new user and link with it
 @receiver(post_save, sender=User)
@@ -26,9 +49,8 @@ def create_user_profile(sender, instance: User, created, raw, **kwargs):
 # Create notification after a post answer is created under a post
 @receiver(post_save, sender=PostAnswer)
 def notify_on_new_postanswer(sender, instance: PostAnswer, created, **kwargs):
-    post_receiver = instance.post.poster
-    content = PostAnswerSerializer(instance).data
-    Notification.objects.create(receiver=post_receiver, content=content)
+    # Use threading as signal is not asynchronous
+    NotificationThread(instance).start()
 
 # Create notification after a chat message
 # TODO Need user to subscribe to a chat room for message notification
