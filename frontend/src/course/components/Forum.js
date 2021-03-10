bb19c6d24970e196708aaa8a4746e755734b1bb4
kimonazj <kimonazj@users.noreply.github.com>
2021-03-10 14:34:12 +0800

    created dummy example

--- frontend/src/course/components/Forum.js
+++ frontend/src/course/components/Forum.js
@@ -15,6 +15,7 @@ import {
   Avatar,
   Space,
   Divider,
+  List
 } from "antd";
 import React,{ Fragment, useState } from "react";
 import {
@@ -25,12 +26,27 @@ import {
   FlagFilled,
 } from "@ant-design/icons";
 
+const sampledata = {
+  avatar : "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+  timestamp : "4hrs",
+  content : "This is a test post card",
+  name : "Jo Biden",
+  comments : [{ name: "Trump",
+    avatar:"https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+    content:"this is a comment content",
+    timestamp:"2hrs"}, { name:"Trump2",
+    avatar:"https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+    content:"this is a comment content2",
+    timestamp:"2hrs"}], //array of comments
+  likes : 10, //the amount of likes
+  dislikes : 11, //dislike amount
+  // Tags container
+  tags : ["#help", "#lol"]
+}
 function Forum() {
   return (
     <Fragment>
-      <PostCard />
-      <PostCard />
-      <PostCard />
+      <PostCard data = {sampledata}/>
     </Fragment>
   );
 }
@@ -53,8 +69,8 @@ function PostCard(props) {
   const name = data.name //"Jo Biden"
   const comments = data.comments //array of comments
   const likeamount = data.likes //the amount of likes
-  const disamount = data.dislike //dislike amount
-  const commentamount = data.comment.length //comment amount
+  const disamount = data.dislikes //dislike amount
+  const commentamount = data.comments.length //comment amount
   // Tags container
   const Tags = data.tags //["#help", "#lol"];
   // For module (pop out comments)
